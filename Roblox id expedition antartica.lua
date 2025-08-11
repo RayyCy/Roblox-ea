3f5cff0c48172ecf3326bcda91c980da15bbed92
RAYYC_COOL <118356301+RayyCy@users.noreply.github.com>
2025-08-11 19:12:37 +0700

    tes

--- Roblox id expedition antartica.lua
+++ Roblox id expedition antartica.lua
@@ -5,127 +5,29 @@ local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/blood
  
 -- Window or baseplate Make tab or section
 
-local Window = Library:NewWindow("Expedition Antartica")
+local Window = Library:NewWindow("Fish It")
  
 -- Section - Section Usefull you don't want ok?
 
 local Section = Window:NewSection("Main")
 
-Section:CreateButton("Get All Badges", function()
-print("Clicked")
-end)
- 
 Section:CreateButton("Infinite Yield", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 wait(5)
 end)
  
 
-Section:CreateToggle("Auto Win (15 Mins)", function(value)
+Section:CreateToggle("Instant Catch", function(value)
 _G.autofarm = value
 while _G.autofarm == true do
-wait(5)
--- Basecamp
-print("Starting")
-wait(0.5)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-5918, -159, -5)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(40)
-local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-4858, -117, 47)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(40)
- local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-3720, 225, 235)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(50)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-3187, 365, 128)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(50)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-2290, 102, -65)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-wait(70)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(-2275, 101, -72)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(50)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(765, 65, -177)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(70)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(1788, 105, -137)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(70)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(3105, 846, -190)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(80)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(3721, 1508, -178)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(80)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(5137, 837, -189)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(90)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(5892, 321, -20)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(40)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(6809, 356, 364)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(40)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(7747, 398, 344)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(40)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(8362, 477, 992)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(50)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(8993, 595, 103)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(45)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(9576, 597, 44)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(30)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(10194, 549, 21)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(60)
-    local tweenInfo = TweenInfo.new(2)
-    local goal = {CFrame = CFrame.new(10993, 549, 107)}
-    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
-    tween:Play()
-wait(10)
-game.Players.LocalPlayer.Character.Head:Destroy()
-wait(5)
+Section:CreateToggle("Instant Catch", function(value)
+_G.autofarm = value
+while _G.autofarm == true do
+while task.wait() do
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_net@0.2.0").net:FindFirstChild("RF/ChargeFishingRod"):InvokeServer(workspace:GetServerTimeNow())
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_net@0.2.0").net:FindFirstChild("RF/RequestFishingMinigameStarted"):InvokeServer(-1.2379989624023438,1)
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_net@0.2.0").net:FindFirstChild("RE/FishingCompleted"):FireServer()
 end
-end)
 
 Section:CreateToggle("Anti Fall Damage", function(value)
 _G.antifall = value
